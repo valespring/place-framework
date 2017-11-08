dcedc96931553491ddce66ad3ec235d82e76ad96
Brian Kelley <bkelly-mac@Brians-NB-MacBook-Pro.local>
2017-11-08 13:02:59 -0600

    testing critical

--- gulp-tasks/scss.js
+++ gulp-tasks/scss.js
@@ -6,6 +6,16 @@ module.exports = function ( gulp, config, $ ) {
 		fixed: config.source.scss + config.master + "-fixed.scss"
 	};
 
+	critical.generate({
+	    inline: true,
+	    base: '/',
+	    src: 'index.html',
+	    dest: 'index.html',
+	    minify: true,
+	    width: 1300,
+	    height: 900
+	});
+
 	// Task Run
 	return () => {
 		gulp.task('scss', function () {
